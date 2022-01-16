95b09bad82c964eefc9b4e185b274e803cebfff8
Алиса Шевчук <alisa000077@gmail.com>
2022-01-16 15:49:14 +0200

    доделанное бинарное дерево с удалением элемента (без ООП)

--- tree_binary/tree_binary/tree_binary.cpp
+++ tree_binary/tree_binary/tree_binary.cpp
@@ -1,20 +1,114 @@
-﻿// tree_binary.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+struct Node
+{
+    int x;
+    Node* l;
+    Node* r;
+};
+Node* tree = NULL;
+void add(int value,Node**tmp)
+{
+    if ((*tmp) == NULL)
+    {
+        (*tmp) = new Node;
+        (*tmp)->x = value;
+        (*tmp)->l = (*tmp)->r = NULL;
+        return;
+    }
+    else
+    {
+        if (value > (*tmp)->x)
+        {
+            add(value, &(*tmp)->r);
+        }
+        else
+        {
+            add(value, &(*tmp)->l);
+        }
+    }
+}
+void show(Node*t)
+{
+    if (t == NULL)
+    {
+        return;
+    }
+    else
+    {
+        show(t->l);
+        std::cout << t->x << "\n";            
+    }
+    show(t->r);
+}
+Node* dtree(Node* root, int key)
+{
+    Node* p, * p2;
 
-#include <iostream>
+    if (!root)
+    {
+        return root;                    
+    }
 
+    if (root->x == key)                 
+    { 
+        if (root->l == root->r)         
+        {
+            free(root);
+            return NULL;
+        }
+        else if (root->l == NULL)        
+        {
+            p = root->r;
+            free(root);
+            return p;
+        }
+        else if (root->r == NULL) 
+        {
+            p = root->l;
+            free(root);
+            return p;
+        }
+        else                               
+        {
+            p2 = root->r;
+            p = root->r;
+            while (p->l) p = p->l;
+            p->l = root->l;
+            free(root);
+            return p2;
+        }
+    }
+    if (root->x < key)
+    {
+        root->r= dtree(root->r, key);
+    }
+    else
+    {
+        root->l = dtree(root->l, key);
+    }
+    return root;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(0, "");
+    int n;
+    int val;
+    std::cout << "Введите кол-во элементов: ";
+    std::cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << "Введите число: ";
+        std::cin >> val;                       
+        add(val, &tree);                 
+    }
+    std::cout << "Полученное дерево:\n";
+    show(tree);
+    std::cout << "\n";
+    int ch;
+    std::cout << "Введите что хотите удалить: ";
+    std::cin >> ch;
+    tree = dtree(tree, ch);
+    std::cout << "\n";
+    show(tree);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
